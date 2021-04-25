2ac06eeaeac97b176c9be7a42684437ec26bdd1e
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-04-25 12:19:25 +0530

    developing seed data

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/blog-website', {
+    useNewUrlParser: true,
+    useCreateIndex : true , 
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error" , console.error.bind(console, "connection error"));
+db.once("open",()=>{
+    console.log("databse connected")
+});
+
+
+app.set('view engine' , 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/' , (Req , res)=>{
-    res.send('done');
+    res.render('home');
 });
+
+app.get('/makeblog',async(req , res)=>{
+    const blog = new Campground({title : 'My city' , description : 'its a good city'});
+    await blog.save();
+    res.send(blog); 
+})
 
 app.listen(3000 , ()=>{
     console.log('port working');
---
+++ models/campground.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema ({
+    title : String,
+    caption : String ,
+    description : String,
+    location : String
+})
+
+module.exports = mongoose.model('Campground' , CampgroundSchema);
