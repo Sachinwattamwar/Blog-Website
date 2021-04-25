594369bb7e11de000a9d5927906cf63dfd810948
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-04-26 02:03:04 +0530

    added ejs mate

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 const { findById } = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -18,6 +19,7 @@ db.once("open",()=>{
 
 const app = express();
 
+app.engine('ejs',ejsMate);
 app.set('view engine' , 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
