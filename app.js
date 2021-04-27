c0729a7bf92c39fd83c07cc7e437fbf56f6ef31a
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-04-27 21:31:44 +0530

    separated blog routes

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const ExpressError = require('./utils/ExpressError');
 const { findById } = require('./models/campground');
 const methodOverride = require('method-override');
 const Review = require('./models/review');
+const campgrounds = require('./routes/campgrounds');
+
 
 mongoose.connect('mongodb://localhost:27017/blog-website', {
     useNewUrlParser: true,
@@ -57,52 +59,20 @@ const validateReview = (req , res , next)=>{
     }
 }
 
+app.use('/blogs', campgrounds);
 
 app.get('/' , (Req , res)=>{
     res.render('home');
 });
 
-app.get('/blogs', catchAsync(async(req , res)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index' , {campgrounds});
-}));
-
-app.get('/blogs/new' , (req , res)=>{
-    res.render('campgrounds/new');
-});
-
-app.post('/blogs',validateCampground ,catchAsync(async(req , res , next)=>{
-    // if(!req.body.campground) throw new ExpressError('Invalid blog data' , 400);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/blogs/${campground._id}`);
-}))
 
 
 
-app.get('/blogs/:id', catchAsync(async(req , res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show', {campground});
-}));
 
-app.get('/blogs/:id/edit', catchAsync(async(req , res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', {campground});
-}));
 
-app.put('/blogs/:id',validateCampground,catchAsync(async (req , res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id , {...req.body.campground} );
-    res.redirect(`/blogs/${campground._id}`);
-}));
 
-app.delete('/blogs/:id', catchAsync(async (req , res)=>{
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/blogs')
-}));
+
+
 
 app.post('/blogs/:id/reviews', validateReview ,catchAsync(async(req , res)=>{
     const campground = await Campground.findById(req.params.id);
---
+++ routes/campgrounds.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const {campgroundSchema , reviewSchema} = require('../schemas.js');
+
+const validateCampground = (req , res , next)=>{
+    const {error} = campgroundSchema.validate(req.body);
+    if(error)
+    {
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg , 400);
+    }
+    else 
+    {
+        next()
+    }
+}
+
+router.get('/', catchAsync(async(req , res)=>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index' , {campgrounds});
+}));
+
+router.get('/new' , (req , res)=>{
+    res.render('campgrounds/new');
+});
+
+router.post('/',validateCampground ,catchAsync(async(req , res , next)=>{
+    // if(!req.body.campground) throw new ExpressError('Invalid blog data' , 400);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/blogs/${campground._id}`);
+}))
+
+router.get('/:id', catchAsync(async(req , res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findById(id).populate('reviews');
+    res.render('campgrounds/show', {campground});
+}));
+
+router.get('/:id/edit', catchAsync(async(req , res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', {campground});
+}));
+
+router.put('/:id',validateCampground,catchAsync(async (req , res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id , {...req.body.campground} );
+    res.redirect(`/blogs/${campground._id}`);
+}));
+
+router.delete('/:id', catchAsync(async (req , res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/blogs')
+}));
+
+
+module.exports = router;
