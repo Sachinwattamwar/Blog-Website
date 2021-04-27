ff74284994f1c88ac1d9a29e1da8d04d4bb3509a
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-04-28 02:40:07 +0530

    adding authencation complete

--- routes/users.js
+++ routes/users.js
@@ -30,8 +30,11 @@ router.get('/login',(req,res)=>{
 });
 
 router.post('/login',passport.authenticate('local',{failureFlash : true , failureRedirect : '/login'}),(req , res)=>{
+
     req.flash('success' , 'welcome back');
-    res.redirect('./blogs');
+    const redirectUrl = req.session.returnTo || '/blogs';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout' , (req , res)=>{
