9026baf8aa847f04feb7df3047cacbd9b82c4f38
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-04-27 23:34:50 +0530

    added session to my app

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
-const catchAsync = require('./utils/catchAsync');
-const {campgroundSchema , reviewSchema} = require('./schemas.js');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
-const { findById } = require('./models/campground');
 const methodOverride = require('method-override');
-const Review = require('./models/review');
+
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -36,6 +32,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret : 'thisissecret',
+    resave : false,
+    saveUninitialized : true ,
+    cookie : {
+        httpOnly : true,
+        expires : Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge : 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 app.use('/blogs', campgrounds);
 app.use('/blogs/:id/reviews' , reviews);
