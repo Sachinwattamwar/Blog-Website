1d45879283accae50bc970cb58c1fc279dfe98ac
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-04-27 23:12:17 +0530

    restructured files

--- app.js
+++ app.js
@@ -10,13 +10,16 @@ const ExpressError = require('./utils/ExpressError');
 const { findById } = require('./models/campground');
 const methodOverride = require('method-override');
 const Review = require('./models/review');
+
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 
 mongoose.connect('mongodb://localhost:27017/blog-website', {
     useNewUrlParser: true,
     useCreateIndex : true , 
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify : false
 });
 const db = mongoose.connection;
 db.on("error" , console.error.bind(console, "connection error"));
@@ -32,63 +35,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
-
-const validateCampground = (req , res , next)=>{
-    const {error} = campgroundSchema.validate(req.body);
-    if(error)
-    {
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg , 400);
-    }
-    else 
-    {
-        next()
-    }
-}
-
-const validateReview = (req , res , next)=>{
-    const {error} = reviewSchema.validate(req.body);
-    if(error)
-    {
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg , 400);
-    }
-    else 
-    {
-        next()
-    }
-}
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/blogs', campgrounds);
+app.use('/blogs/:id/reviews' , reviews);
 
 app.get('/' , (Req , res)=>{
     res.render('home');
 });
 
-
-
-
-
-
-
-
-
-
-app.post('/blogs/:id/reviews', validateReview ,catchAsync(async(req , res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    review.save();
-    campground.save();
-    res.redirect(`/blogs/${campground._id}`);
-}))
-
-app.delete('/blogs/:id/reviews/:reviewId' , catchAsync(async(req , res)=>{
-    const {id , reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id,{$pull:{reviews : reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/blogs/${id}`);
-}))
 
 app.all('*',(req , res , next)=>{
     next(new ExpressError('Page not found',404));
