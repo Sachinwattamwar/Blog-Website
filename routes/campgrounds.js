0bc8e84b8af5a1a9f83bbcda13b7a5a690592988
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-04-28 15:24:25 +0530

    added additional middlewares

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const {campgroundSchema , reviewSchema} = require('../schemas.js');
-const {isLoggedIn} = require('../middleware');
+const {isLoggedIn , isAuthor , validateCampground} = require('../middleware');
 
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 
-const validateCampground = (req , res , next)=>{
-    const {error} = campgroundSchema.validate(req.body);
-    if(error)
-    {
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg , 400);
-    }
-    else 
-    {
-        next()
-    }
-}
+
 
 router.get('/', catchAsync(async(req , res)=>{
     const campgrounds = await Campground.find({});
@@ -51,7 +38,7 @@ router.get('/:id',catchAsync(async(req , res)=>{
     res.render('campgrounds/show', {campground});
 }));
 
-router.get('/:id/edit', isLoggedIn,catchAsync(async(req , res)=>{
+router.get('/:id/edit', isLoggedIn,isAuthor ,catchAsync(async(req , res)=>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
     if(!campground)
@@ -59,32 +46,18 @@ router.get('/:id/edit', isLoggedIn,catchAsync(async(req , res)=>{
         req.flash('error' , 'cannot find the blog');
         return res.redirect('/blogs');
     }
-    if(!campground.author.equals(req.user._id)){
-        req.flash('error' , 'You do not own the permission to edit this');
-        return res.redirect(`/blogs/${id}`);
-    }
     res.render('campgrounds/edit', {campground});
 }));
 
-router.put('/:id',isLoggedIn ,validateCampground,catchAsync(async (req , res)=>{
+router.put('/:id',isLoggedIn ,isAuthor ,validateCampground,catchAsync(async (req , res)=>{
     const {id} = req.params;
-    const campground = await Campground.findById(id);
-    if(!campground.author.equals(req.user._id)){
-        req.flash('error' , 'You do not own the permission to edit this');
-        return res.redirect(`/blogs/${id}`);
-    }
-    const camp = await Campground.findByIdAndUpdate(id , {...req.body.campground} );
+    const campground = await Campground.findByIdAndUpdate(id , {...req.body.campground} );
     req.flash('success' , 'Successfully edited the blog');
     res.redirect(`/blogs/${campground._id}`);
 }));
 
-router.delete('/:id', isLoggedIn,catchAsync(async (req , res)=>{
+router.delete('/:id', isLoggedIn,isAuthor ,catchAsync(async (req , res)=>{
     const {id} = req.params;
-    const campground = await Campground.findById(id);
-    if(!campground.author.equals(req.user._id)){
-        req.flash('error' , 'You do not own the permission to edit this');
-        return res.redirect(`/blogs/${id}`);
-    }
     await Campground.findByIdAndDelete(id);
     req.flash('success' , 'Successfully deleted a blog');
     res.redirect('/blogs')
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,19 +5,9 @@ const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const {campgroundSchema , reviewSchema} = require('../schemas.js');
 const Review = require('../models/review');
+const {validateReview} = require('../middleware');
 
-const validateReview = (req , res , next)=>{
-    const {error} = reviewSchema.validate(req.body);
-    if(error)
-    {
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg , 400);
-    }
-    else 
-    {
-        next()
-    }
-}
+
 
 router.post('/', validateReview ,catchAsync(async(req , res)=>{
     const campground = await Campground.findById(req.params.id);
