eef2ff80e4eeb7fa6ffc4a4067828ab94593f4e1
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-05-01 01:15:42 +0530

    arranged all routes more systematically

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,14 @@ const User = require('../models/user');
 const passport = require('passport');
 const users = require('../controllers/users');
 
-router.get('/register',users.renderRegister)
 
-router.post('/register',catchAsync(users.register));
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/login',users.renderLogin);
-
-router.post('/login',passport.authenticate('local',{failureFlash : true , failureRedirect : '/login'}), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local',{failureFlash : true , failureRedirect : '/login'}), users.login);
 
 router.get('/logout' , users.logout);
 
