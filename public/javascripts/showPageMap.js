0df2d702b0985386aad0d28a2dcb7e45788a8353
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-05-05 21:55:03 +0530

    map is working

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,9 +1,13 @@
 
 
-        mapboxgl.accessToken = mapToken;
-        const map = new mapboxgl.Map({
-        container: 'map', 
-        style: 'mapbox://styles/mapbox/streets-v11', 
-        center: [-74.5, 40], 
-        zoom: 9 
-        });
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+container: 'map', 
+style: 'mapbox://styles/mapbox/streets-v11', 
+center: campground.geometry.coordinates, 
+zoom: 8
+});
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map)
