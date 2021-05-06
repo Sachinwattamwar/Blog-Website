8848c71a64a2d33da8204389dc80c4a46a2a9b65
Sachin Wattamwar <sachin.w2000@gmail.com>
2021-05-06 12:13:01 +0530

    final changes

--- app.js
+++ app.js
@@ -114,6 +114,8 @@ app.use((err , req , res , next)=>{
     res.status(statusCode).render('error' , {err});
 });
 
-app.listen(3000 , ()=>{
-    console.log('port working');
+const port = process.env.PORT || 3000;
+
+app.listen(port , ()=>{
+    console.log(`port working  ${port}`);
 });
